33ff4a4e870b9365a8bc77f31bb99a40bacd09aa
Gabriel <gbri2586@gmail.com>
2025-04-05 12:07:35 -0300

    feat: amenieties + store

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,16 +1,20 @@
-import { FaStar, FaRegHeart } from 'react-icons/fa6';
+import { FaStar, FaRegHeart, FaWifi,FaCar } from 'react-icons/fa6';
+import { MdAccessible } from "react-icons/md";
+import { MdOutlineChildCare } from "react-icons/md";
 
 import './Main.scss';
 
+
 export default function Main() {
+
   return (
     <>
-      <main>
-        <div className="container">
+      <main className="home container">
           <picture className='banner-container'>
             <img src="./banner.png" alt="" />
           </picture>
           <section className="section-store">
+            <div className='content'>
             <div className="store">
               <picture className="logo-container">
                 <img src="./barbearia.png" alt="" />
@@ -21,7 +25,9 @@ export default function Main() {
                   5.0<b className='svg'> <FaStar /></b>
                 </span>
               </div>
+              
             </div>
+            
             <div className="store-menu">
               <button className="like-button">
                 <FaRegHeart />
@@ -30,8 +36,47 @@ export default function Main() {
                 Agendar agora
               </a>
             </div>
+            </div>
+           <nav className='store-nav'>
+            <ul className='services-list'>
+              <li className='service-item'>Serviços</li>
+              <li className='service-item'>Profissionais</li>
+              <li className='service-item'>Sobre</li>
+              <li className='service-item'>Fidelidades</li>
+              <li className='service-item'>Produtos</li>
+              <li className='service-item'>Assinaturas</li>
+              <li className='service-item'>Avaliações</li>
+            </ul>
+           </nav>
+           <p className='store-description'>Barbearia por assinatura. Lorem ipsum dolor sit amet.</p>
+
+           <section className='amenities'>
+            <h2 className='title'>Comodidades</h2>
+            <p>
+              Clique no item para obter informações
+            </p>
+
+            <ul className='amenities-list'>
+              <li className='amenity-item'>
+                <span className='icon'><FaWifi/></span>
+                Wi-fi
+              </li>
+              <li className='amenity-item'>
+                <span className='icon'><FaCar/></span>
+              Estacionamento
+              </li><li className='amenity-item'>
+                <span className='icon'><MdAccessible/></span>
+                Acessibilidade
+              </li><li className='amenity-item'>
+                <span className='icon'><MdOutlineChildCare/></span>
+                Atende crianças
+              </li>
+            </ul>
+           </section>
+            
           </section>
-        </div>
+          
+         
       </main>
     </>
   );
